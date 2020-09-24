5455c7e9c622b7427269be645a872a01796eda6d
Mohit29061999 <68787888+Mohit29061999@users.noreply.github.com>
2020-09-24 09:08:45 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>       //include file for input/output
+using namespace std;      //ham standard namespace use krenege
+
+
+//hello world ka code h
+//main return int
+//main kuch nhi return kar rha hota toh void
+int main(){  //start in flowchart
+      int a;  //variable named a
+      int b;   //variable named b
+
+      cout << "Enter the values to be sumed" << '\n';  //user ko pta chalega ki usko kya krna h
+      cin>>a;   //cin - same as read a (pseudocode)
+      cin>>b;   //b ki value user se maang rahe h
+
+      int sum =0;  //initialse
+        sum=a+b;   //variable for storing a+b  //update or assignment
+      cout << sum << '\n'; //'\n' to go to new line
+
+
+
+
+return 0;   //stop in flowchart or exit in pseudocode
+}
+
